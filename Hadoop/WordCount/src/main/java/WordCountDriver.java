3fbf5d1b5d62d98fd7a2f2ebae0d92376cbaf1dc
Bohan Gao <bohangao@ALEX-PC.northamerica.corp.microsoft.com>
2020-05-19 21:52:16 -0700

    WordCount practice

---
+++ Hadoop/WordCount/src/main/java/WordCountDriver.java
@@ -0,0 +1,41 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+
+public class WordCountDriver {
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+        //1.get job instance
+        Job job = Job.getInstance(new Configuration());
+
+        //2.set jar location - reflection
+        job.setJarByClass(WordCountDriver.class);
+
+        //3.set mapper and reducer class - reflection
+        job.setMapperClass(WordCountMapper.class);
+        job.setReducerClass(WordCountReducer.class);
+
+        //4.set output format of map stage - reflection
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(IntWritable.class);
+
+        //5.set output format of reduce stage - reflection
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        //6.set MR input and output file path
+        FileInputFormat.setInputPaths(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        //7.submit job
+        boolean result = job.waitForCompletion(true);
+
+        //8.exit with job completion code
+        System.exit(result ? 0 : 1);
+    }
+}
