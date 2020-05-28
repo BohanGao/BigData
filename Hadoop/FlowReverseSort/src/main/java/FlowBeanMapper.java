0eb59c136fbd5719b6791be693763ae2c113c73f
Bohan Gao <bohangao@ALEX-PC.northamerica.corp.microsoft.com>
2020-05-27 23:50:08 -0700

    customized sort on flow bean sum flow

---
+++ Hadoop/FlowReverseSort/src/main/java/FlowBeanMapper.java
@@ -0,0 +1,19 @@
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class FlowBeanMapper extends Mapper<LongWritable, Text, FlowBean, Text> {
+    FlowBean k = new FlowBean();
+    Text v = new Text();
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        String[] fields = value.toString().split(("\\s+"));
+
+        k.set(Long.parseLong(fields[fields.length-3]), Long.parseLong(fields[fields.length-2]));
+        v.set(fields[1]);
+        context.write(k, v);
+    }
+}
